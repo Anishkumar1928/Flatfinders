3159d20fbaa8ac5b300b522f0810dfeed3e45c62
Anish kumar <mr.anish.kmr@gmail.com>
2024-10-28 10:15:18 +0530

    update

--- index.py
+++
@@ -1,8 +0,0 @@
-from wsgi import app
-import os
-
-# Check if the database exists before starting the app
-if os.path.exists("/tmp/rental_app.db"):
-    print("Database file found!")
-else:
-    print("Database file not found. Creating a new one...")
