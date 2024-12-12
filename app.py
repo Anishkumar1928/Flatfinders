760f2d1baad0956357fe51fa484d9d46c9a6ba6d
Anish kumar <mr.anish.kmr@gmail.com>
2024-12-13 01:46:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -247,14 +247,8 @@ def deleteproperty():
 @app.rout("/getpropertybypincode",methods=["POST"])
 def getpropertybypincode():
     data = request.get_json()
-    property_id = data.get("property_id")
-    try:
-        propertydetails = db.read_property(property_id)
-        property_photo=db.read_property_picture(property_id)
-        return jsonify(propertydetails=propertydetails,property_photo=property_photo), 200
-    except Exception as e:
-       print(f"propert not found: {e}")
-       return jsonify({"msg": "propert not found"}), 500
+    property = data.get("property_id")
+    return jsonify({"msg":property})
 
     
 
