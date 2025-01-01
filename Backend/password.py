d2cf2c277db695d79e4044abb2541142ea8e5dc8
Anish kumar <mr.anish.kmr@gmail.com>
2025-01-01 11:57:21 +0530

    update

--- Backend/password.py
+++
@@ -1,3 +0,0 @@
-from werkzeug.security import generate_password_hash, check_password_hash
-
-print(check_password_hash(" scrypt:32768:8:1$OSZMhmCWDQeYs5X0$0f267c9a7e677ade0096dd5b8c5ff457ca72a704e83733e7e5c941fbae52d9539a4ed9a154252632ecc52b5b626445cf9e4355827a3ddd1876438977034b7818"))
