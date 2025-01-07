826bc6f586ecaea50f4e7dc1be463108c53e7ef7
Anish kumar <mr.anish.kmr@gmail.com>
2025-01-08 00:47:48 +0530

    Update app.py

--- Backend/app.py
+++ Backend/app.py
@@ -336,8 +336,8 @@ def forgot_password():
         # Generate a reset token
         token = generate_reset_token()
         reset_tokens[email] = token
-        
-        send_email(email,f"Password Reset Token= {token}")
+
+        send_email(email,f"Flatfinderapp Reset Token",f"Password Reset Token= {token}")
 
         return jsonify({"msg": "Password reset token sent to your email."}), 200
 
