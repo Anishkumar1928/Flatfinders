85209654b5f80431c81ffb35e41a393c35589fcc
Anish kumar <mr.anish.kmr@gmail.com>
2025-01-08 00:11:53 +0530

    Update app.py

--- Backend/app.py
+++ Backend/app.py
@@ -19,7 +19,7 @@ app.config["MAIL_SERVER"] = "smtp.gmail.com"
 app.config["MAIL_PORT"] = 587
 app.config["MAIL_USE_TLS"] = True
 app.config["MAIL_USERNAME"] = "flatfindersapp@gmail.com"  # Replace with your email
-app.config["MAIL_PASSWORD"] = "anish8084"         # Replace with your email password
+app.config["MAIL_PASSWORD"] = "Anish8084@"         # Replace with your email password
 mail = Mail(app)
 jwt = JWTManager(app)
 
